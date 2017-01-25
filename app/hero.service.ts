560af6ce5584e323a2fff4f4b5f1aef0000a7643
Hansel, Eric R <erhansel@keystonecollects.com>
2017-01-25 13:30:01 -0500

    Update implementation.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -11,21 +11,35 @@ export class HeroService {
     constructor(private http: Http) { }
 
     private heroesUrl = 'api/heroes';  // URL to web api
+    private headers = new Headers({ 'Content-Type': 'application/json' });
 
     getHeroes(): Promise<Hero[]> {
         return this.http.get(this.heroesUrl)
             .toPromise()
             .then(response => response.json().data as Hero[])
             .catch(this.handleError);
+    }
 
+
+    getHero(id: number): Promise<Hero> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error); // for demo purposes only
         return Promise.reject(error.message || error);
-    }
-    getHero(id: number): Promise<Hero> {
-        return this.getHeroes()
-            .then(heroes => heroes.find(hero => hero.id === id));
     }
 }
